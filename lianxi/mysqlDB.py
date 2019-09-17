03861cbfa9d2ee03bb807100a4ffffd4a21cbd01
Ckq001 <1432391766@qq.com>
2019-09-17 18:59:31 +0800

    数据库
    Signed-off-by: Ckq001 <1432391766@qq.com>

--- lianxi/mysqlDB.py
+++ lianxi/mysqlDB.py
@@ -10,8 +10,8 @@ mysql = ReadConfig()
 
 class MysqlDb:
     def __init__(self):
-        self.host = mysql.Database("host1")
-        self.user = mysql.Database("user1")
+        self.host = mysql.Database("host")
+        self.user = mysql.Database("user")
         self.passwd = mysql.Database("passwd")
         self.database = mysql.Database("database")
         self.db = MySQLdb.connect(self.host,self.user,self.passwd,self.database,charset="utf8")
